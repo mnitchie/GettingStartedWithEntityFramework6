290cc4ad600814bcfcac094827ea0fa9a5b8ae11
Mike Nitchie <mikenitchie@gmail.com>
2015-12-13 14:12:42 -0500

    Fixing foreign key

--- NinjaDomain.Classes/NinjaEquipment.cs
+++ NinjaDomain.Classes/NinjaEquipment.cs
@@ -1,10 +1,13 @@
-﻿namespace NinjaDomain.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NinjaDomain.Classes
 {
 	public class NinjaEquipment
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public EquipmentType Type { get; set; }
+		[Required]
 		public Ninja Ninja { get; set; }
 	}
 }

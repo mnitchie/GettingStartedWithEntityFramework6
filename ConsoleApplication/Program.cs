38ee5a706d11ce0d435a5067d77a555abc09be9b
Mike Nitchie <mikenitchie@gmail.com>
2015-12-13 15:08:18 -0500

    Add some code for the console app.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NinjaDomain.Classes;
+using NinjaDomain.DataModel;
+using System;
 
 namespace ConsoleApplication
 {
@@ -10,6 +8,41 @@ namespace ConsoleApplication
 	{
 		static void Main( string[] args )
 		{
+			InsertNinja();
+			// InsertMultipleNinjas();
+			//SimpleNinjaQueries();
+			//QueryAndUpdateNinja();
+			//DeleteNinja();
+			//RetrieveDataWithFind();
+			//RetrieveDataWithStoredProc();
+			//DeleteNinjaWithKeyValue();
+			//DeleteNinjaViaStoredProcedure();
+			//QueryAndUpdateNinjaDisconnected();
+
+			//InsertNinjaWithEquipment();
+			//SimpleNinjaGraphQuery();
+			//ProjectionQuery();
+			//QueryAndUpdateNinjaDisconnected();
+
+			//ReseedDatabase();
+			Console.ReadKey();
+		}
+
+		private static void InsertNinja()
+		{
+			var ninja = new Ninja {
+				Name = "SampsonSan",
+				ServedInOniwaban = false,
+				DateOfBirth = new DateTime( 2008, 1, 28 ),
+				ClanId = 1
+
+			};
+			using ( var context = new NinjaContext() )
+			{
+				context.Database.Log = Console.WriteLine;
+				context.Ninjas.Add( ninja );
+				context.SaveChanges();
+			}
 		}
 	}
 }

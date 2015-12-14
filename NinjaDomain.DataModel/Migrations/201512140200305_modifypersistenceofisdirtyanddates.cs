1e390b1a836a419b2edcae730c721e399f5b4b04
Mike Nitchie <mikenitchie@gmail.com>
2015-12-13 21:01:26 -0500

    meaningless migration.

---
+++ NinjaDomain.DataModel/Migrations/201512140200305_modifypersistenceofisdirtyanddates.cs
@@ -0,0 +1,16 @@
+namespace NinjaDomain.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class modifypersistenceofisdirtyanddates : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
